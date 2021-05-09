62c6d7ce270b8d32bd62134296e3036e0b3db65e
michael1904lin <michael1904lin@gmail.com>
2021-05-09 17:04:18 +0800

    player c script 2

--- 2D_Rockman/Assets/script/player.cs
+++ 2D_Rockman/Assets/script/player.cs
@@ -38,6 +38,21 @@ public class player : MonoBehaviour
     private void Update()
     {
         Move();
+        Jump();
+    }
+
+    [Header("判斷地板碰撞的位移與半徑")]
+    public Vector3 groundOffset;
+    public float groundRadius = 0.2f;
+
+    // 繪製圖式 - 輔助編輯時的圖形線條
+    private void OnDrawGizmos()
+    {
+        // 1. 指定顏色
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        // 2. 繪製圖形
+        // transform 可以抓到此腳本停一層的變形元件
+        Gizmos.DrawSphere(transform.position + groundOffset, groundRadius);
     }
     #endregion
 
@@ -49,11 +64,33 @@ public class player : MonoBehaviour
     {
         // 1. 要抓到玩家按下左右鍵的資訊 INPUT
         float h = Input.GetAxis("Horizontal");
-        print("水平的值:" + h);
+        //print("水平的值:" + h);
         // 2. 使用左右鍵的資訊控制角色移動
         // 剛體.加速度 = 二維向量(水平 * 速度, 一蒸的時間, 指定 回原本的 Y 軸加速度)
         // 一蒸的時間 - 解決不同效能的裝置速度差問題
         rig.velocity = new Vector2(h * speed * Time.deltaTime, rig.velocity.y);
+    }
+
+    /// <summary>
+    /// 跳躍
+    ///</summary>
+    private void Jump()
+    {
+        // 如果 玩家 按下 空白鍵 就 往上跳躍
+        // 判斷式 C#
+        // 傳回值為布林值的方法可以當成布林值使用
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jump));
+        }
+    }
+
+    ///<summary>
+    ///開槍
+    ///</summary>
+    private void Fire()
+    {
+
     }
 }
 #endregion
